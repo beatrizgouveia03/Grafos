02ca026e6d3b831c22004704a766dfd37284bc2b
Beatriz <72415136+beatrizgouveia03@users.noreply.github.com>
2024-10-20 15:46:36 -0300

    Updated code to example entry file

--- Exercícios/Módulo 1/listaAdjacenciaBia.cpp
+++ Exercícios/Módulo 1/listaAdjacenciaBia.cpp
@@ -6,34 +6,37 @@ using namespace std;
 
 //Leitura da entrada do grafo e atualização da lista
 void lerGrafo(map<int, vi>& graph){
-    int qnt , x, y;
-    cout << "Informe a quantidade de conexões que o grafo possui: " << endl;
+    int vertices, conexoes, x, y;
+    
+    //Quantidade de vértices e conexoes da matriz
+    cin >> vertices >> conexoes;
 
-    cin >> qnt;
+    for(auto i{1u}; i<=vertices; ++i) graph[i];
 
-    cout << "Informe as conexões existentes entre os vértices, uma por linha:" << endl;
-    cout << "Siga o exemplo: \"4 5\" indica que os vértices 4 e 5 são conectados" << endl;
 
-    while(qnt > 0){
+    while(conexoes > 0){
         cin >> x >> y;
 
         graph[x].push_back(y);
         graph[y].push_back(x);
 
-        qnt--;
+        conexoes--;
     }
 
 }
 
 //Impressão da lista de adjacência resultante
-void imprimirGrafo(map<int, vi> graph, int x){
+void imprimirGrafo(map<int, vi> graph){
     cout << "Lista de Adjacência resultante: " << endl; 
     cout << "---------------------------------" << endl;
 
-    for(auto i{1u}; i<=x; ++i){
+    for(auto i{1u}; i<=graph.size(); ++i){
         cout << i << " ->";
-        for(auto j{0u}; j < graph[i].size(); ++j){
-            cout << " " <<  graph[i][j];
+        if(graph[i].size() == 0) cout << " Sem adjacentes";
+        else {
+            for(auto j{0u}; j < graph[i].size(); ++j){
+                cout << " " <<  graph[i][j];
+            }
         }
         cout << endl;
     }
@@ -41,18 +44,12 @@ void imprimirGrafo(map<int, vi> graph, int x){
 }
 
 int main(){
-    int x;
-    
-    cout << "Informe a quantidade de vértices da matriz: " << endl;
-    cin >> x;
-
     //Inicialização da lista de adjacência do grafo
-    map<int, vi> graph;
+    map<int, vi> listaAdjacencia;
+    
+    lerGrafo(listaAdjacencia);
 
-
-    lerGrafo(graph);
-
-    imprimirGrafo(graph, x);
+    imprimirGrafo(listaAdjacencia);
 
     return 0;
 }
