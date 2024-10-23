4c131c17e1a934f152824ac3c38409989296771a
Beatriz <72415136+beatrizgouveia03@users.noreply.github.com>
2024-10-23 16:48:01 -0300

    Added the operation.cpp with all the headers from the operation.hpp

---
+++ Exercícios/Módulo_1/source/operations.cpp
@@ -0,0 +1,59 @@
+#include "operations.hpp"
+
+using namespace sml;
+
+void Operations::adjListToAdjMatrix(map<int, vector<int>> adjList){
+    /*TO-DO*/
+}
+void Operations::adjMatrixToAdjList(vector<vector<int>> adjMatrix){
+    /*TO-DO*/
+}
+void Operations::calculateDegrees(Graph g){
+    /*TO-DO*/
+}
+void Operations::adjacentsVertex(Graph g, int u, int v){
+    /*TO-DO*/
+}
+void Operations::countVertices(Graph g){
+    /*TO-DO*/
+}
+void Operations::countEdges(Graph g){
+    /*TO-DO*/
+}
+void Operations::connected(Graph g){
+    /*TO-DO*/
+}
+void Operations::bipartite(Graph g){
+    /*TO-DO*/
+}
+void Operations::dfs(Graph g, int v){
+    /*TO-DO*/
+}
+void Operations::bfs(Graph g, int v){
+    /*TO-DO*/
+}
+void Operations::articulationsAndBlocks(Graph g){
+    /*TO-DO*/
+}
+//THE MATH AND SEARCH OPERATIONS FROM DIGRAPHS
+void Operations::subjacenteGraph(Digraph d){
+    /*TO-DO*/
+}
+void Operations::incMatrixToDirectStar(vector<vector<int>> digraphIncMatrix){
+    /*TO-DO*/
+}
+void Operations::directStarToIncMatrix(/*Dont know*/){
+    /*TO-DO*/
+}
+void Operations::adjMatrixToIndirectStar(vector<vector<int>> digraphAdjMatrix){
+    /*TO-DO*/
+}
+void Operations::indirectStarToAdjMatrix(/*Dont know*/){
+    /*TO-DO*/
+}
+void Operations::dfs(Digraph d){
+    /*TO-DO*/
+}
+void Operations::application(){
+    /*TO-DO*/
+}
