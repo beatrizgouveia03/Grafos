863cd71e69bc67a3ad231062958669946fb53010
Beatriz <72415136+beatrizgouveia03@users.noreply.github.com>
2025-01-05 10:59:46 -0300

    refactor: changed degrees logic

--- Projetos/Route_in_Graphs/source/algorithms/hierholzerPaths.cpp
+++ Projetos/Route_in_Graphs/source/algorithms/hierholzerPaths.cpp
@@ -10,22 +10,27 @@ void Simulation::hierholzerPaths (void){
     // Calcular graus de entrada e saída
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            in[j] += aux[i][j];
-            out[i] += aux[i][j];
+            (aux[i][j] > 0) ? out[i]++, in[j]++ : 0;
         }   
     }
     
     // Calcular graus de cada vértice
     vector<int> degrees(n, 0);                  //!< Vetor de graus
     for (int i = 0; i < n; i++) {
-        degrees[i] = in[i] - out[i];
+        degrees[i] = out[i] - in[i];
+    }
+
+    // testando tabela de graus
+    for(auto i{0}; i<n; ++i){
+        cout << i+1 << " : " << degrees[i] << endl; 
     }
 
     // Verifica se o grafo é euleriano
     if(degrees[0] != 1 || degrees[n-1] != -1){
-        cout << "Não há caminho euleriano." << endl;
+        cout << "Nao ha caminho euleriano." << endl;
         return;
     }
+
 
     // Verificar a condição de existência de um caminho euleriano em um digrafo
     int count = 0;
@@ -37,12 +42,10 @@ void Simulation::hierholzerPaths (void){
     }
 
     if (count > 2) {
-        cout << "Não há caminho euleriano." << endl;
+        cout << "Nao ha caminho euleriano." << endl;
         return;
     }
 
     // Inicializar pilha para armazenar caminho euleriano
-
-
-};
+}   
 
