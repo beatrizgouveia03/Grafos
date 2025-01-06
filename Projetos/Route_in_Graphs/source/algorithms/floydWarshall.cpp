8c6d0ed2063b78446f13c9bacdd4f25ef90822cb
Beatriz <72415136+beatrizgouveia03@users.noreply.github.com>
2025-01-06 14:26:50 -0300

    chore: adjusted terminal printing matrixes

--- Projetos/Route_in_Graphs/source/algorithms/floydWarshall.cpp
+++ Projetos/Route_in_Graphs/source/algorithms/floydWarshall.cpp
@@ -1,7 +1,4 @@
 #include "simulation.h" 
-#include <vector>
-#include <limits>
-#include <iostream>
 
 using namespace sml;
 
@@ -15,9 +12,10 @@ void Simulation::floydWarshall() {
     const int INF = std::numeric_limits<int>::max();  // Define o valor de infinito
 
     int n = this->graph.n;  // Número de vértices no grafo
-    std::vector<std::vector<int>> adj = this->graph.adj;  // Matriz de adjacência
-    std::vector<std::vector<int>> dist(n, std::vector<int>(n, INF));  // Matriz de distâncias
-    std::vector<std::vector<int>> pred(n, std::vector<int>(n, -1));  // Matriz de predecessores
+    vector<vector<int>> adj = this->graph.adj;  // Matriz de adjacência
+    vector<vector<int>> dist(n, vector<int>(n, INF));  // Matriz de distâncias
+    vector<vector<int>> pred(n, vector<int>(n, -1));  // Matriz de predecessores
+    map<int, string> dic = this->graph.dictionary;    // Lista que mantém o nome de cada vértice
 
     // Inicializa as distâncias e predecessores
     for (int i = 0; i < n; ++i) {
@@ -47,24 +45,106 @@ void Simulation::floydWarshall() {
     }
 
     // Exibe as distâncias mínimas
-    std::cout << " Matriz de distâncias mínimas entre os vertices:" << std::endl;
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            if (dist[i][j] == INF) {
-                std::cout << "INF ";  // Exibe INF para distâncias infinitas
-            } else {
-                std::cout << dist[i][j] << " "; 
-            }
-        }
-        std::cout << std::endl;
+    cout << "Matriz de distancias minimas entre os vertices:" << endl << endl;
+  // cout << "      ";
+
+  // for (int i = 0; i < n; i++)
+  // {
+  //     cout << i+1;
+  //     if(i+1 < 10) cout << " ";
+  //     cout << " ";
+  // }
+  // cout << endl;
+
+  // for (int i = 0; i < n; ++i) {
+  //     if(i+1 < 10) cout << " ";
+  //     cout << i+1 << " ";
+
+  //     for (int j = 0; j < n; ++j) {
+  //         if (dist[i][j] == INF) {
+  //             cout << "INF ";  // Exibe INF para distâncias infinitas
+  //         } else {
+  //             cout << " ";
+  //             if(dist[i][j] < 10 && dist[i][j] >= 0) cout << " ";
+  //             cout << dist[i][j] << " "; 
+  //         }
+  //     }
+  //     cout << endl;
+  // }
+   
+    cout << "  \\  "; 
+    for (int i{0}; i<n; ++i) {
+      if(dic[i].length() == 1) cout << " ";
+      cout << dic[i] <<"  ";
     }
+    cout << endl;
+
+    cout << "    ";
+    for (int i{0}; i<n; ++i) {
+      cout << "____";
+    }
+    cout << endl;
+
+    for(int i{0}; i<n; ++i){
+      if(dic[i].length() == 1) cout << " ";
+      cout << dic[i] << "| ";
+      for(int j{0}; j<n; ++j){
+        if (dist[i][j] == INF) {
+            cout << "INF ";  // Exibe INF para distâncias infinitas
+        } else {
+            if(dist[i][j] < 10) cout << "  ";
+            else if(dist[i][j] < 100) cout << " ";
+            cout << dist[i][j] << " ";
+        }
+      }
+      cout << endl;
+    }
+    cout << endl;
 
     // Exibe os predecessores
-    std::cout << " Matriz de predecessores para reconstrucao de caminhos:" << std::endl;
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            std::cout << pred[i][j] << " "; 
-        }
-        std::cout << std::endl;
+    cout << "Matriz de predecessores para reconstrucao de caminhos:" << endl << endl;
+    //cout << "   ";
+    //
+    //for (int i = 0; i < n; i++)
+    //{
+    //    cout << i+1;
+    //    if(i+1 < 10) cout << " ";
+    //    cout << " ";
+    //}
+    //cout << endl;
+//
+    //for (int i = 0; i < n; ++i) {
+    //    if(i+1 < 10) cout << " ";
+    //    cout << i+1 << " ";
+//
+    //    for (int j = 0; j < n; ++j) {
+    //        if(pred[i][j] < 10 && pred[i][j] >= 0) cout << " ";
+    //        cout << pred[i][j] << " "; 
+    //    }
+    //    cout << endl;
+    //}
+    cout << "  \\  "; 
+    for (int i{0}; i<n; ++i) {
+      if(dic[i].length() == 1) cout << " ";
+      cout << dic[i] <<"  ";
     }
+    cout << endl;
+
+    cout << "    ";
+    for (int i{0}; i<n; ++i) {
+      cout << "____";
+    }
+    cout << endl;
+
+    for(int i{0}; i<n; ++i){
+      if(dic[i].length() == 1) cout << " ";
+      cout << dic[i] << "| ";
+      for(int j{0}; j<n; ++j){
+        if(pred[i][j] < 10 && pred[i][j]>=0) cout << "  ";
+        else if(pred[i][j] < 100) cout << " ";
+        cout << pred[i][j] << " ";
+      }
+      cout << endl;
+    }
+    cout << endl;
 }
